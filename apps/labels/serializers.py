77b45aa1a2ed4452ac55001a90273fb3956114b0
Israr Ali <israr.ali@datumbrain.com>
2025-01-18 12:02:37 +0500

    Apply Unique Constraint On Label Name And Owner Fields In Serializers

--- apps/labels/serializers.py
+++ apps/labels/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 from apps.labels.models import Labels
 
 class LabelSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')    
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())  
     
     class Meta:
         model = Labels
         fields = ['id', 'name', 'owner']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Labels.objects.all(), 
+                fields=('name', 'owner')
+            )
+        ] 
         
