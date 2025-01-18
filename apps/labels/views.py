163547752bab98a95f9b1d8f684c6f3b5bb823f7
Israr Ali <israr.ali@datumbrain.com>
2025-01-18 11:26:14 +0500

    Apply JWT Authentication On Label Views

--- apps/labels/views.py
+++ apps/labels/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from apps.labels.models import Labels
 from apps.labels.serializers import LabelSerializer
@@ -9,6 +10,7 @@ class LabelViewSet(viewsets.ModelViewSet):
     queryset = Labels.objects.all()
     serializer_class = LabelSerializer
     permission_classes = [IsAuthenticated, IsOwner]
+    authentication_classes = [JWTAuthentication]
     
     def get_queryset(self):
         return Labels.objects.filter(owner=self.request.user)
