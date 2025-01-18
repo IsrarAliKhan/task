bcce6a8bc32d5221964b3b23e46422d040bc372b
Israr Ali <israr.ali@datumbrain.com>
2025-01-18 18:51:30 +0500

    Implement Many-to-Many Relation Between Tasks And Labels

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from apps.labels.models import Labels
+
 class Tasks(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     is_complete = models.BooleanField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    labels = models.ManyToManyField(Labels, related_name='tasks')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- apps/tasks/serializers.py
+++ apps/tasks/serializers.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
 
 from apps.tasks.models import Tasks
+from apps.labels.models import Labels
+from apps.labels.serializers import LabelSerializer
 
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    labels = LabelSerializer(read_only=True, many=True)
+    label_ids = serializers.PrimaryKeyRelatedField(queryset=Labels.objects.all(), write_only=True, many=True, source="labels")
     
     class Meta:
         model = Tasks
-        fields = ['id', 'title', 'description', 'is_complete', 'owner']
+        fields = ['id', 'title', 'description', 'is_complete', 'owner', 'labels', 'label_ids']
+        
+    def validate(self, data):
+        request = self.context.get('request')
+        labels = data.get('labels')
+        for label in labels:
+            if label.owner != request.user:
+                raise serializers.ValidationError("No Label matches the given label id: {0}".format(label.id))  
+        return data
         
