c2460673210ecbc1c113673b98dfeea90c372e52
Israr Ali <israr.ali@datumbrain.com>
2025-01-19 21:15:39 +0500

    Make Description Field Nullable And Set Default To Completion Field

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -5,8 +5,8 @@ from apps.labels.models import Labels
 
 class Tasks(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    is_complete = models.BooleanField()
+    description = models.TextField(default=None, blank=True, null=True)
+    is_complete = models.BooleanField(default=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
     labels = models.ManyToManyField(Labels, related_name='tasks')
     created_at = models.DateTimeField(auto_now_add=True)
